c548df13247bc2ea1a0ce590d0af0f65ed46918b
Rgee Bautista <rgee12104590@gmail.com>
2017-07-21 08:28:20 +0800

    Drag and drop completed

--- dnd.js
+++ dnd.js
@@ -2,9 +2,38 @@ $(function (){
 	
 	$('#draggables').children().draggable({
 		containment: '.jumbotron',
+		revert: 'invalid',
 		helper: 'clone'
 	});
 	
-	$('#d1, #d2, #d3').draggable('option', 'stack', '.ui-draggable');
+	$('#question1').droppable({drop: checkAnswer});
+
+
+	$('#question2').droppable( {drop: checkAnswer} );
+	
+	
+	$('#question3').droppable({drop: checkAnswer});
+	
 	
 });
+
+function checkAnswer(event, ui)
+{
+	answerID = ui.draggable.attr('id');  //answer1
+	questionID = this.id; // question1
+	filteredAnswerID = answerID.match(/[0-9]+/g)[0]; // 1
+	filteredQuestionID = questionID.match(/[0-9]+/g)[0]; //1
+	$('#' + questionID).val( ui.draggable.data("answer") ); // Answer 1 New
+	
+	//Can add code to accept or reject or shiny colors when right or wrong
+	
+	if(filteredAnswerID == filteredQuestionID){
+			$('#' + questionID).addClass('answerCorrect');
+	}
+	else{
+		$('#' + questionID).addClass('answerWrong');
+	}
+}
+
+
+
